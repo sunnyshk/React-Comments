e6004957ef20a7698cb3d1de463e862c378814c0
Juned <87689793+mafiacoders@users.noreply.github.com>
2022-05-24 12:24:39 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Comments from '../components/Comments'
+import './App.css'
+
+function App() {
+
+  return (
+   <Comments currentUserId="1"/>
+  )
+}
+
+export default App
